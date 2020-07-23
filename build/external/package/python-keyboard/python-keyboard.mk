624477c45c6c490a22f441f9e47fb1ebc67854b1
donno2048 <61805754+donno2048@users.noreply.github.com>
2020-07-23 17:23:30 +0300

    Create python-keyboard.mk

---
+++ build/external/package/python-keyboard/python-keyboard.mk
@@ -0,0 +1,3 @@
+PYTHON_KEYBOARD_SITE = https://files.pythonhosted.org/packages/55/88/287159903c5b3fc6d47b651c7ab65a54dcf9c9916de546188a7f62870d6d
+PYTHON_KEYBOARD_SETUP_TYPE = setuptools
+$(eval $(python-package))
