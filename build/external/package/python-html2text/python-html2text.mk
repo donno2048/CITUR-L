cfd8397d661be13b19ffdd2df0757d8b3dddf506
donno2048 <61805754+donno2048@users.noreply.github.com>
2020-07-23 17:38:56 +0300

    Create python-html2text.mk

---
+++ build/external/package/python-html2text/python-html2text.mk
@@ -0,0 +1,4 @@
+PYTHON_HTML2TEXT_SOURCE = text2html-0.0.2.dev0.tar.gz
+PYTHON_HTML2TEXT_SITE = https://files.pythonhosted.org/packages/2f/b7/28076c50fd047eb5f5ec3cd794087744c8e1af35458f913f981fb61a66cf
+PYTHON_HTML2TEXT_SETUP_TYPE = setuptools
+$(eval $(python-package))
