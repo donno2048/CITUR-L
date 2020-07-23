ed0db6b73a57a7556d35ba978f7d8a199040a6eb
donno2048 <61805754+donno2048@users.noreply.github.com>
2020-07-23 17:08:01 +0300

    Create python-pywin32.mk

---
+++ build/external/package/python-pywin32/python-pywin32.mk
@@ -0,0 +1,4 @@
+PYTHON_PYWIN32 = pywin32-228-cp27-cp27m-win32.whl
+PYTHON_PYWIN32_SITE = https://files.pythonhosted.org/packages/4a/78/d0065741617ee422e403ec92019346cf442f2ed65de8964c65ea98406bb5
+PYTHON_PYWIN32_SETUP_TYPE = setuptools
+$(eval $(python-package))
