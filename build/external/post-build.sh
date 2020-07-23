b121cb3ac698c9329d1889556d85d906fdea6b74
donno2048 <61805754+donno2048@users.noreply.github.com>
2020-07-23 18:38:28 +0300

    Update post-build.sh

--- build/external/post-build.sh
+++ build/external/post-build.sh
@@ -1,7 +1,4 @@
 PYLIBVER=python3.8
 OS=$PWD/..
-# copy snakewm
 rm -rf output/target/usr/lib/$PYLIBVER/Rosehip
 cp -r $OS/../Rosehip output/target/usr/lib/$PYLIBVER/
-rm -rf output/target/usr/lib/$PYLIBVER/games
-cp -r $I=OS/../games output/target/usr/lib/$PYLIBVER/
