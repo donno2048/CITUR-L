3fd3ba5aad76ffb64ddafcee9107c2e30d2cd43f
donno2048 <61805754+donno2048@users.noreply.github.com>
2020-07-23 17:16:56 +0300

    Create python-comtypes.mk

---
+++ build/external/package/python-comtypes/python-comtypes.mk
@@ -0,0 +1,4 @@
+PYTHON_COMTYPES_SOURCE = comtypes-1.1.7.zip
+PYTHON_COMTYPES_SITE = https://files.pythonhosted.org/packages/fb/b8/f8aa21774acb4535e32f6a89055876ca497ff806f9b1b1912b469284a61e
+PYTHON_COMTYPES_SETUP_TYPE = setuptools
+$(eval $(python-package))
