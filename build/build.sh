a0cdd12f545354fcc0768d09300dc8d1efcc7a9b
donno2048 <61805754+donno2048@users.noreply.github.com>
2020-07-23 15:22:06 +0300

    Create build.sh

---
+++ build/build.sh
@@ -0,0 +1,9 @@
+BUILDROOT_VERSION=2020.05
+OS=$PWD
+git clone -b $BUILDROOT_VERSION https://github.com/buildroot/buildroot.git buildroot_x86-64 --depth 1
+cd buildroot_x86-64
+make BR2_EXTERNAL="$OS/external" "x86-64_defconfig"
+make
+cd $OS
+cp buildroot_x86-64/output/images/rootfs.iso9660 Rosehip_x86-64.iso
+echo "Rosehip_x86-64.iso SUCCESS :)"
