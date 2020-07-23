29667fdf7a1ea1085d80da39b4598c95a4657eea
donno2048 <61805754+donno2048@users.noreply.github.com>
2020-07-23 17:43:47 +0300

    Create python-chromedriver-autoinstaller.mk

---
+++ build/external/package/chromedriver-autoinstaller/python-chromedriver-autoinstaller.mk
@@ -0,0 +1,4 @@
+PYTHON_CHROMEDRIVER_AUTOINSTALLER_SOURCE = chromedriver-autoinstaller-0.2.2.tar.gz
+PYTHON_CHROMEDRIVER_AUTOINSTALLER_SITE = https://files.pythonhosted.org/packages/9a/00/cf64825db96a05797baadfdb2bca905d02de1182c9c229688144ee4f6ead
+PYTHON_CHROMEDRIVER_AUTOINSTALLER_SETUP_TYPE = setuptools
+$(eval $(python-package))
