e15279e631bfb2e4c861844de94fd1e32aab0e54
donno2048 <61805754+donno2048@users.noreply.github.com>
2020-07-23 17:31:51 +0300

    Update python-Js2Py.mk

--- build/external/package/python-Js2Py/python-Js2Py.mk
+++ build/external/package/python-Js2Py/python-Js2Py.mk
@@ -1,4 +1,4 @@
-PYTHON_PYGAME_GUI_SOURCE = Js2Py-0.70.tar.gz
-PYTHON_PYGAME_GUI_SITE =https://files.pythonhosted.org/packages/50/ca/96809659b66f06f10a65a73023d5a2cdc1f9376f080473ea9397593e2b7b
-PYTHON_PYGAME_GUI_SETUP_TYPE = setuptools
+PYTHON_JS2PY_SOURCE = Js2Py-0.70.tar.gz
+PYTHON_JS2PY_SITE =https://files.pythonhosted.org/packages/50/ca/96809659b66f06f10a65a73023d5a2cdc1f9376f080473ea9397593e2b7b
+PYTHON_JS2PY_SETUP_TYPE = setuptools
 $(eval $(python-package))
