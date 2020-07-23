a7fe64ad4a8731edc801f0264cd7a963f9125033
donno2048 <61805754+donno2048@users.noreply.github.com>
2020-07-23 17:39:41 +0300

    Update python-requests.mk

--- build/external/package/python-requests/python-requests.mk
+++ build/external/package/python-requests/python-requests.mk
@@ -1,4 +1,4 @@
-PYTHON_PYGAME_GUI_SOURCE = requests-2.24.0.tar.gz
-PYTHON_PYGAME_GUI_SITE = https://files.pythonhosted.org/packages/da/67/672b422d9daf07365259958912ba533a0ecab839d4084c487a5fe9a5405f
-PYTHON_PYGAME_GUI_SETUP_TYPE = setuptools
+PYTHON_REQUESTS_SOURCE = requests-2.24.0.tar.gz
+PYTHON_REQUESTS_SITE = https://files.pythonhosted.org/packages/da/67/672b422d9daf07365259958912ba533a0ecab839d4084c487a5fe9a5405f
+PYTHON_REQUESTS_SETUP_TYPE = setuptools
 $(eval $(python-package))
