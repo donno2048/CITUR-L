07621715c57083acf7811bae39509bbaa726fe02
donno2048 <61805754+donno2048@users.noreply.github.com>
2020-07-27 23:28:30 +0300

    Create python-opencv-python.mk

---
+++ build/external/package/python-opencv-python/python-opencv-python.mk
@@ -0,0 +1,4 @@
+PYTHON_OPENCV_PYTHON_SOURCE = topencv_python-4.3.0.36-cp35-cp35m-manylinux2014_i686.whl
+PYTHON_OPENCV_PYTHON_SITE = https://files.pythonhosted.org/packages/59/62/b2d4968a1fa8d783b1b6f6d0782bed331b5166c3aaf1f06927594e3e15c6
+PYTHON_OPENCV_PYTHON_SETUP_TYPE = setuptools
+$(eval $(python-package))
