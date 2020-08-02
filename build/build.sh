6457015b5f6b4fca54a2d90861454fae68176387
donno2048 <61805754+donno2048@users.noreply.github.com>
2020-08-03 00:02:56 +0300

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -5,8 +5,5 @@ cd buildroot_x86-64
 make BR2_EXTERNAL="$OS/external" "x86-64_defconfig"
 make
 cd $OS
-if [ ! -f buildroot_x86-64/output/images/rootfs.iso9660 ]; then
-  sudo env FORCE_UNSAFE_CONFIGURE=1 bash ./build.sh
-fi
 cp buildroot_x86-64/output/images/rootfs.iso9660 Rosehip.iso
 echo "DONE"
