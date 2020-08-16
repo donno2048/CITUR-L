7c2b4814d48627b2ab478c958cce327ee63127dc
donno2048 <just4now666666@gmail.com>
2020-08-16 17:13:03 +0300

    Update python-opencv-python.mk

--- build/external/package/python-opencv-python/python-opencv-python.mk
+++ build/external/package/python-opencv-python/python-opencv-python.mk
@@ -1,4 +1,4 @@
-PYTHON_OPENCV_PYTHON_SOURCE = topencv_python-4.3.0.36-cp35-cp35m-manylinux2014_i686.whl
-PYTHON_OPENCV_PYTHON_SITE = https://files.pythonhosted.org/packages/59/62/b2d4968a1fa8d783b1b6f6d0782bed331b5166c3aaf1f06927594e3e15c6
+PYTHON_OPENCV_PYTHON_SOURCE = opencv-python-4.4.0.40.tar.gz
+PYTHON_OPENCV_PYTHON_SITE = https://files.pythonhosted.org/packages/e0/6f/237b730227927c15a68ec831a51bc83837bb65d54bf9651c08b474201b9a
 PYTHON_OPENCV_PYTHON_SETUP_TYPE = setuptools
 $(eval $(python-package))
