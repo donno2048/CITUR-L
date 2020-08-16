691d0bc7ecc07d6f7349476c779852811b3110fc
donno2048 <just4now666666@gmail.com>
2020-08-16 10:40:00 +0300

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,6 @@
 BUILDROOT_VERSION=2020.05
 OS=$PWD
-git clone -b $BUILDROOT_VERSION https://github.com/buildroot/buildroot.git buildroot_x86-64 --depth 1
+git clone -b $BUILDROOT_VERSION https://github.com/donno2048/buildroot.git buildroot_x86-64 --depth 1
 cd buildroot_x86-64
 make BR2_EXTERNAL="$OS/external" "x86-64_defconfig"
 make
