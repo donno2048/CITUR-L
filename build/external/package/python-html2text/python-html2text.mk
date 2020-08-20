bf145007b1b6bebc21ecc4609c84a2d4fc52866f
Elisha Hollander <just4now666666@gmail.com>
2020-08-20 10:55:40 +0300

    Update python-html2text.mk

--- build/external/package/python-html2text/python-html2text.mk
+++ build/external/package/python-html2text/python-html2text.mk
@@ -1,4 +1,4 @@
-PYTHON_HTML2TEXT_SOURCE = c77b580c94d1a9e0145f23cc4472993d
+PYTHON_HTML2TEXT_SOURCE = html2text-2020.1.16.tar.gz
 PYTHON_HTML2TEXT_SITE = https://files.pythonhosted.org/packages/6c/f9/033a17d8ea8181aee41f20c74c3b20f1ccbefbbc3f7cd24e3692de99fb25
 PYTHON_HTML2TEXT_SETUP_TYPE = setuptools
 $(eval $(python-package))
