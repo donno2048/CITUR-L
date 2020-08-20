dbf3a6b4f51fe67e5867f45b877f4832bc8cd713
Elisha Hollander <just4now666666@gmail.com>
2020-08-20 15:08:20 +0300

    Delete python-opencv-python.mk

--- build/external/package/python-opencv-python/python-opencv-python.mk
+++
@@ -1,4 +0,0 @@
-PYTHON_OPENCV_PYTHON_SOURCE = opencv-python-4.4.0.40.tar.gz
-PYTHON_OPENCV_PYTHON_SITE = https://files.pythonhosted.org/packages/e0/6f/237b730227927c15a68ec831a51bc83837bb65d54bf9651c08b474201b9a
-PYTHON_OPENCV_PYTHON_SETUP_TYPE = setuptools
-$(eval $(python-package))
