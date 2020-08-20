26b3e718cb4b49a59058bc4869bbfac94434742e
Elisha Hollander <just4now666666@gmail.com>
2020-08-20 16:18:34 +0300

    Delete python-requests.mk

--- build/external/package/python-requests/python-requests.mk
+++
@@ -1,4 +0,0 @@
-PYTHON_REQUESTS_SOURCE = requests-2.24.0.tar.gz
-PYTHON_REQUESTS_SITE = https://files.pythonhosted.org/packages/da/67/672b422d9daf07365259958912ba533a0ecab839d4084c487a5fe9a5405f
-PYTHON_REQUESTS_SETUP_TYPE = setuptools
-$(eval $(python-package))
