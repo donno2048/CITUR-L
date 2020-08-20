b9248e5062c3285a61c56afd7ed62358f5b85c1e
Elisha Hollander <just4now666666@gmail.com>
2020-08-20 11:03:18 +0300

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,5 @@
 OS=$PWD
-git clone https://github.com/buildroot/buildroot.git
+git clone https://github.com/donno2048/buildroot.git
 mv buildroot buildroot_x86-64
 cd buildroot_x86-64
 make BR2_EXTERNAL="$OS/external" "x86-64_defconfig"
