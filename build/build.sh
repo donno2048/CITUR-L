51fb3fb54f61eb32041b913f1b2d456f91f66a56
Elisha Hollander <just4now666666@gmail.com>
2020-08-23 19:12:04 +0300

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -3,6 +3,10 @@ git clone https://github.com/donno2048/buildroot.git
 mv buildroot buildroot_x86-64
 cd buildroot_x86-64
 make BR2_EXTERNAL="$OS/external" "x86-64_defconfig"
+cd ..
+chmod 777 -R buildroot_x86-64/
+chmod 777 -R external/
+cd buildroot_x86-64
 make
 cd $OS
 cp buildroot_x86-64/output/images/rootfs.iso9660 Rosehip.iso
