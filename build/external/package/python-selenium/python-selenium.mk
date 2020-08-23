d8575704a8c8f6d6ba9499aafe82317bbe055b21
Elisha Hollander <just4now666666@gmail.com>
2020-08-23 14:15:44 +0300

    Delete python-selenium.mk

--- build/external/package/python-selenium/python-selenium.mk
+++
@@ -1,4 +0,0 @@
-PYTHON_SELENIUM_SOURCE = selenium-3.141.0.tar.gz
-PYTHON_SELENIUM_SITE = https://files.pythonhosted.org/packages/ed/9c/9030520bf6ff0b4c98988448a93c04fcbd5b13cd9520074d8ed53569ccfe
-PYTHON_SELENIUM_SETUP_TYPE = setuptools
-$(eval $(python-package))
