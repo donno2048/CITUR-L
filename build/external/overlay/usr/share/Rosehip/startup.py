ac3cba6d8bdf498589bfa2be416390c1d3d38c72
Elisha Hollander <just4now666666@gmail.com>
2020-08-25 11:29:23 +0300

    Update startup.py

--- build/external/overlay/usr/share/Rosehip/startup.py
+++ build/external/overlay/usr/share/Rosehip/startup.py
@@ -1,8 +1,5 @@
-class Command:
-    def run(self):raise NotImplementedError
-    def __repr__(self):return self.run()
 class ROSEHIP(Command):
-    def run(self):
-        from Rosehip.os import OS
-        return OS().run()
+    def run(self):from Rosehip.os import OS;return OS().run()
+    def __repr__(self):return self.run()
 rosehip = ROSEHIP()
+def bash(string):from os import popen;print(popen(string).read())
