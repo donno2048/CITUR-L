6911e001f3551a186606e09650b7409409a6b60b
Elisha Hollander <just4now666666@gmail.com>
2020-08-26 17:26:24 +0300

    Update startup.py

--- build/external/overlay/usr/share/Rosehip/startup.py
+++ build/external/overlay/usr/share/Rosehip/startup.py
@@ -1,5 +1,5 @@
 class ROSEHIP():
-    def run(self):from Rosehip.os import OS;return OS().run()
+    def run(self):from Rosehip import pc;pc()
     def __repr__(self):return self.run()
 rosehip = ROSEHIP()
 def bash(string):from os import popen;print(popen(string).read())
