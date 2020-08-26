e0649c1059ade0d94d5682f50a3f77331e744e28
Elisha Hollander <just4now666666@gmail.com>
2020-08-26 23:37:45 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -13,7 +13,7 @@ sudo apt-get install rsync
 sudo apt-get install python3
 mv build/external/overlay/usr/share/Rosehip/startup.py build/external/overlay/usr/share
 yes | rm -r build/external/overlay/usr/share/Rosehip
-cp Rosehip build/external/overlay/usr/share/Rosehip
+cp -a Rosehip/. build/external/overlay/usr/share/Rosehip
 mv build/external/overlay/usr/share/startup.py build/external/overlay/usr/share/Rosehip
 cd build/
 sudo env FORCE_UNSAFE_CONFIGURE=1 bash ./build.sh
