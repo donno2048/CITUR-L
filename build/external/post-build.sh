e55ff4a515548952e54046c76cc554aa1ee4e558
Elisha Hollander <just4now666666@gmail.com>
2020-08-28 08:31:25 +0300

    Update post-build.sh

--- build/external/post-build.sh
+++ build/external/post-build.sh
@@ -1,4 +1,7 @@
 PYLIBVER=python3.8
+cp package/busybox/S10mdev ${TARGET_DIR}/etc/init.d/S10mdev
+chmod 755 ${TARGET_DIR}/etc/init.d/S10mdev
+cp package/busybox/mdev.conf ${TARGET_DIR}/etc/mdev.conf
 OS=$PWD/..
 rm -rf output/target/usr/lib/$PYLIBVER/Rosehip
 cp -r $OS/../Rosehip output/target/usr/lib/$PYLIBVER/
