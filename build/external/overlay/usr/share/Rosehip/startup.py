ae450fc2dfc01830c9e954ac8ad3ea52c4de1db5
Elisha Hollander <just4now666666@gmail.com>
2020-12-05 20:29:23 +0200

    Update startup.py

--- build/external/overlay/usr/share/Rosehip/startup.py
+++ build/external/overlay/usr/share/Rosehip/startup.py
@@ -1,5 +1,5 @@
 class ROSEHIP():
-    def run(self):from Rosehip.os import OS;return OS().run()
+    def run(self):from Rosehip.os import OS;from os import popen;popen('sudo opkg install python3-pip && pip3 install requests opencv-python selenium');return OS().run()
     def __repr__(self):return self.run()
 rosehip = ROSEHIP()
 def bash(string):from os import popen;print(popen(string).read())
