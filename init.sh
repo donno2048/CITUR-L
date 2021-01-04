8f6fe20accf44f70625a74a74c2b0482b18e9a9b
Elisha Hollander <just4now666666@gmail.com>
2021-01-04 14:24:20 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,16 +1,7 @@
 sudo add-apt-repository universe
 sudo apt-get update
 sudo dpkg --configure -a
-sudo apt-get install git
-sudo apt-get install git-all
-sudo apt-get install bc
-sudo apt-get install mercurial
-sudo apt-get install unzip
-sudo apt-get install build-essential
-sudo apt-get install wget
-sudo apt-get install cpio
-sudo apt-get install rsync
-sudo apt-get install python3
+sudo apt-get install git git-all bc mercurial unzip build-essential wget cpio rsync python3
 git clone https://github.com/donno2048/Rosehip-L.git
 mv Rosehip-L Rosehip
 cd build/
